53a1b544cb632401e729e7c04c5801c1ee69dab9
Matthew Vilim <mvilim2@illinois.edu>
2015-07-28 17:15:07 -0700

    A

---
+++ src/cpu/reg_file.h
@@ -0,0 +1,12 @@
+/*
+ * Core 8086 Copyright (C) 2014 Matthew Vilim
+ *
+ * src/cpu/reg_file.h
+ */
+
+ #ifndef REG_FILE_H
+ #define REG_FILE_H
+
+void reg_file_init(reg_file_t *reg_file);
+
+ #endif
